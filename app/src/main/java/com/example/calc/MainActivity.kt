d4140e2371acc598fd16c134488d4d9116635201
AndriaBegiashvili <92532950+AndriaBegiashvili@users.noreply.github.com>
2021-11-06 15:37:52 +0400

    bego

---
+++ app/src/main/java/com/example/calc/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.example.calc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var resultTextView: TextView
+    private var operand: Double = 0.0
+    private var operation: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        resultTextView = findViewById(R.id.resultTextView)
+    }
+
+    fun numberClick(clickedView: View){
+        if (clickedView is TextView){
+            var result = resultTextView.text.toString()
+            val number = clickedView.text.toString()
+
+            if (result == "0"){
+                result = ""
+            }
+
+
+            resultTextView.text = result + number
+
+
+
+        }
+
+
+    }
+
+    fun operationClick(clickedView: View){
+        if (clickedView is TextView) {
+            val result = resultTextView.text.toString()
+            if (result.isNotEmpty()){
+                operand = result.toDouble()
+            }
+            operation = clickedView.text.toString()
+            resultTextView.text = ""
+
+
+
+
+        }
+    }
+    fun equalsClick(clickedView: View){
+        val secOperandText: String = resultTextView.text.toString()
+        var secOperand: Double = 0.0
+        if(secOperandText.isNotEmpty()){
+            secOperand = secOperandText.toDouble()
+        }
+
+
+
+        when(operation){
+            "+" -> resultTextView.text = (operand + secOperand).toString()
+            "-" -> resultTextView.text = (operand - secOperand).toString()
+            "×" -> resultTextView.text = (operand * secOperand).toString()
+            "÷" -> resultTextView.text = (operand / secOperand).toString()
+            "." -> resultTextView.text = "."
+            "x²" -> resultTextView.text = (operand * operand).toString()
+            "%" -> resultTextView.text = ( (operand / 100) * secOperand).toString()
+            "\uD83D\uDDD1" -> resultTextView.text = ""
+
+        }
+
+
+
+    }
+    fun deleteClick(clickedView: View) {
+        val text = resultTextView.text.toString()
+        if (text.isNotEmpty()) {
+            resultTextView.text = text.dropLast(1)
+        }
+    }
+
+
+
+
+
+
+}
